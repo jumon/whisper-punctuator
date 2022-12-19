56c515b52ecdfab55065fef4d6e7c18b01b11389
jumon <jumon.nozaki@gmail.com>
2022-12-19 22:08:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# whisper-punctuator
+Zero-shot punctuation insertion using the [Whisper](https://github.com/openai/whisper) speech recognition model:
+* No additional training required
+* Works on any language that Whispers supports
+* Can change the style of punctuation by using a prompt
+
+Have you ever wanted to fine-tune a Whisper model using public data, but the data doesn't have punctuation? If so, this script is for you! It allows you to insert punctuation into unpunctuated text using the Whisper model in a zero-shot fashion, using a pair of unpunctuated text and audio files.
+
+## Quick Start
+To use the script, first install [Whisper](https://github.com/openai/whisper) and its dependencies. See the instructions [here](https://github.com/openai/whisper#setup) for more details.
+
+Run the following command to insert punctuation into text using a text and audio file pair as input:
+```
+python insert_punctuation.py --audio <path-to-audio-file> --text <text-to-be-punctuated>
+```
+Note that the audio needs to be shorter than 30 seconds; if it is longer, the first 30 seconds will be used.
+
+The default setting treats `,` `.` `?` as punctuation. To change the punctuation characters, use the `--punctuations` flag and specify a list of characters. For example, to treat `,` `.` `?` `!` as punctuation, run:
+```
+python insert_punctuation.py --audio <path-to-audio-file> --text <text-to-be-punctuated> --punctuations ",.?!"
+```
+To handle languages other than English, you can use the `--language` flag to specify the language. For example, to insert punctuation for a Japanese text and treat `。` `、` as punctuation, run:
+```
+python insert_punctuation.py --audio <path-to-audio-file> --text <text-to-be-punctuated> --language ja --punctuations "。、"
+```
+To change the style of punctuation, use the `--initial-prompt` flag to specify a prompt. This will make the model more likely insert punctuation in the style of the prompt. For example, to insert punctuation after every word (though this is not recommended), run:
+```
+python insert_punctuation.py --audio <path-to-audio-file> --text <text-to-be-punctuated> --initial-prompt "hello, how, are, you, today?"
+```
+
+For all available options, run:
+```
+python insert_punctuation.py --help
+```
+
+## How does it work?
+Whisper is an automatic speech recognition (ASR) model trained on a massive amount of labeled audio data collected from the internet.
+The data used in its training contains punctuation, so the model learns to recognize punctuation as well.
+This allows Whisper to insert punctuation into a text given an audio and text pair.
+To insert punctuation, the audio is first input into the encoder of the model to generate the encoder hidden states.
+Then, the decoder of the model processes each token in the text one by one, in an autoregressive fashion, along with the encoder hidden states. After each token, the model predicts the output probability for each punctuation character. If the highest probability is above a specified threshold (specified using the --min-prob flag), the punctuation is inserted after the token.
+
+## Limitations
+- The results are dependent on the dataset used to train Whisper, which is not publicly available. Punctuation marks that are rare in the training data may not be recognized well.
+- Since the Whisper decoder generates tokens in a left-to-right fashion, it cannot see future tokens when predicting the punctuation after the current token. This can lead to problems in some cases, such as when `まーはい` (uh yes, where まー means uh and はい means yes) in Japanese is often punctuated as `ま、ーはい` instead of `まー、はい` This is because the model cannot see the future `ー` (Japanese long vowel) when predicting the punctuation after `ま`, and `ま` itself also means `uh` in Japanese. We circumvent this problem by preventing the model from inserting punctuation after `ー` that is specified by the `--punctuation-suppressing-chars` flag. However, this is not a perfect solution and the model may still suffer from problems due to its left-to-right decoding nature.
+- If the model fails to insert punctuation when it should, it may enter a "no-punctuation" mode and not insert any further punctuation in the text. This is another issue caused by the left-to-right decoding nature. To mitigate this problem, you can use the `--initial-prompt` flag to induce the model to enter a "punctuation" mode.
+- The --min-prob and --initial-prompt flags may need to be fine-tuned to get the best results, depending on the data.
+- The current implementation does not allow for punctuation marks that consist of multiple tokens according to the Whisper tokenizer.
+- If you want to fine-tune a Whisper model using publiclly available data, you may want to ensure that the data is not only punctuated but also truecased. This script does not perform truecasing, but it may be possible to achievet this using similar (but probably more complicated) techinques.
---
+++ insert_punctuation.py
@@ -0,0 +1,229 @@
+import argparse
+import json
+import unicodedata
+from dataclasses import dataclass
+from typing import List
+
+import torch
+import whisper
+from tqdm import tqdm
+from whisper import Whisper
+from whisper.audio import N_FRAMES, log_mel_spectrogram, pad_or_trim
+from whisper.tokenizer import LANGUAGES, TO_LANGUAGE_CODE, Tokenizer, get_tokenizer
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Zero-shot punctuation insertion using Whisper")
+    parser.add_argument("--audio", type=str, help="Path to an audio file")
+    parser.add_argument("--text", type=str, help="Text to be punctuated")
+    parser.add_argument(
+        "--json", type=str, help="Path to a jsonl file containing audio paths and texts"
+    )
+    parser.add_argument(
+        "--punctuations", type=str, default=",.?", help="List of punctuations to insert"
+    )
+    parser.add_argument(
+        "--language",
+        type=str,
+        default="en",
+        choices=sorted(LANGUAGES.keys()) + sorted([k.title() for k in TO_LANGUAGE_CODE.keys()]),
+        help="Language of the text",
+    )
+    parser.add_argument(
+        "--min-probability",
+        type=float,
+        default=0.2,
+        help="Minimum probability to insert a punctuation",
+    )
+    parser.add_argument(
+        "--initial-prompt", type=str, default=None, help="Optional text to provide as a prompt"
+    )
+    parser.add_argument(
+        "--punctuation-suppressing-chars",
+        type=str,
+        default="ー",
+        help=(
+            "Do not insert punctuations `before` these characters."
+            "Default: ー (Japanese long vowel). This is useful to prevent typical Japanese"
+            "punctuation insertion errors such as まーはい -> ま、ーはい。"
+        ),
+    )
+    parser.add_argument("--unicode-normalize", action="store_true", help="Normalize unicode")
+    parser.add_argument("--device", default="cuda", help="Device to use for inference")
+    parser.add_argument(
+        "--model",
+        default="large",
+        choices=whisper.available_models(),
+        help="Name of the Whisper model to use",
+    )
+    parser.add_argument("--verbose", action="store_true", help="Verbose output")
+    parser.add_argument(
+        "--output", type=str, default="output/prediction.json", help="Path to the output file"
+    )
+    return parser
+
+
+@dataclass
+class Record:
+    audio_path: str
+    text: str
+
+
+def create_records(args: argparse.Namespace) -> List[Record]:
+    if args.audio is not None and args.text is not None:
+        records = [Record(args.audio, args.text)]
+    elif args.json is not None:
+        records = read_json(args.json)
+    else:
+        raise ValueError("Either --audio and --text or --json must be specified")
+
+    if args.unicode_normalize:
+        records = [
+            Record(record.audio_path, unicodedata.normalize("NFKC", record.text))
+            for record in records
+        ]
+    return records
+
+
+def read_json(path: str) -> List[Record]:
+    records = []
+    with open(path) as f:
+        for line in f:
+            data = json.loads(line)
+            records.append(Record(audio_path=data["audio_path"], text=data["text"]))
+    return records
+
+
+def write_json(records: List[Record], output: str):
+    with open(output, "w") as f:
+        for record in records:
+            data = {"audio_path": record.audio_path, "text": record.text}
+            f.write(json.dumps(data, ensure_ascii=False) + "\n")
+
+
+def get_tokens(tokenizer: Tokenizer, characters: str) -> torch.Tensor:
+    tokens = []
+    for char in characters:
+        encoded = tokenizer.encode(char)
+        if len(encoded) > 1:
+            raise ValueError(f"Character {char} is not a single token")
+        tokens.append(encoded[0])
+    return torch.tensor(tokens)
+
+
+def get_initial_tokens(tokenizer: Tokenizer, model: Whisper, initial_prompt: str) -> torch.Tensor:
+    initial_tokens = []
+    if initial_prompt is not None:
+        initial_tokens.append(tokenizer.sot_prev)
+        initial_prompt = tokenizer.encode(" " + initial_prompt.strip())
+        initial_tokens.extend(initial_prompt[-(model.dims.n_text_ctx // 2 - 1) :])
+
+    initial_tokens.extend(tokenizer.sot_sequence_including_notimestamps)
+    return torch.tensor(initial_tokens)
+
+
+def cleanup_hooks(hooks: List[torch.utils.hooks.RemovableHandle]) -> None:
+    for hook in hooks:
+        hook.remove()
+
+
+def calculate_audio_features(audio_path: str, model: Whisper) -> torch.Tensor:
+    mel = log_mel_spectrogram(audio_path)
+    segment = pad_or_trim(mel, N_FRAMES).to(model.device)
+    return model.embed_audio(segment.unsqueeze(0))
+
+
+@torch.no_grad()
+def predict_punctuations(
+    audio_path: str,
+    text: str,
+    model: Whisper,
+    tokenizer: Tokenizer,
+    initial_tokens: torch.Tensor,
+    punctuation_tokens: torch.Tensor,
+    punctuation_suppressing_tokens: torch.Tensor,
+    min_probability: float,
+) -> str:
+    audio_features = calculate_audio_features(audio_path, model)
+    kv_cache, hooks = model.install_kv_cache_hooks()
+    input_tokens = initial_tokens.unsqueeze(0).to(model.device)
+    original_text_tokens = torch.tensor(tokenizer.encode(text)).to(model.device)
+    len_original_text = len(original_text_tokens)
+    punctuated_tokens = []
+
+    for i in range(len_original_text):
+        token = original_text_tokens[i]
+        punctuated_tokens.append(token.item())
+        if (
+            i < len_original_text - 1
+            and original_text_tokens[i + 1] in punctuation_suppressing_tokens
+        ):
+            continue
+
+        input_tokens = torch.cat([input_tokens, token.view(1, 1)], dim=1)
+        logits = model.decoder(input_tokens, audio_features, kv_cache=kv_cache)
+        probabilities = logits.squeeze(0)[-1].softmax(dim=0)
+        max_punctuation_probability, max_punctuation_index = torch.max(
+            probabilities[punctuation_tokens], dim=0
+        )
+
+        if max_punctuation_probability > min_probability:
+            selected_punctuation = punctuation_tokens[max_punctuation_index]
+            punctuated_tokens.append(selected_punctuation.item())
+            # update kv_cache
+            model.decoder(selected_punctuation.view(1, 1), audio_features, kv_cache=kv_cache)
+
+        input_tokens = torch.tensor([[]], dtype=torch.long).to(model.device)
+
+    cleanup_hooks(hooks)
+    punctuated_text = tokenizer.decode(punctuated_tokens)
+    return punctuated_text
+
+
+def main():
+    args = get_parser().parse_args()
+    records = create_records(args)
+
+    tokenizer = get_tokenizer(
+        multilingual=".en" not in args.model, language=args.language, task="transcribe"
+    )
+    try:
+        punctuation_tokens = get_tokens(tokenizer, args.punctuations)
+    except ValueError:
+        raise ValueError("Punctuations must be single tokens")
+
+    try:
+        punctuation_suppressing_tokens = get_tokens(tokenizer, args.punctuation_suppressing_chars)
+    except ValueError:
+        raise ValueError("punctuation-suppressing-chars must be single tokens")
+
+    punctuation_tokens = punctuation_tokens.to(args.device)
+    punctuation_suppressing_tokens = punctuation_suppressing_tokens.to(args.device)
+
+    model = whisper.load_model(args.model, args.device)
+    initial_tokens = get_initial_tokens(tokenizer, model, args.initial_prompt)
+
+    punctuated_records = []
+    for record in tqdm(records):
+        punctuated_text = predict_punctuations(
+            audio_path=record.audio_path,
+            text=record.text,
+            model=model,
+            tokenizer=tokenizer,
+            initial_tokens=initial_tokens,
+            punctuation_tokens=punctuation_tokens,
+            punctuation_suppressing_tokens=punctuation_suppressing_tokens,
+            min_probability=args.min_probability,
+        )
+        punctuated_records.append(Record(record.audio_path, punctuated_text))
+
+        if args.verbose:
+            tqdm.write(record.audio_path)
+            tqdm.write(f"  Original:   {record.text}")
+            tqdm.write(f"  Punctuated: {punctuated_text}")
+
+    write_json(punctuated_records, args.output)
+
+
+if __name__ == "__main__":
+    main()
